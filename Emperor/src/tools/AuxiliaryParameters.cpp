de60b5caa010e990c5ad7aed80322f42c55e6f9a
Stefan Sicklinger <stefan.sicklinger@tum.de>
2013-09-09 15:50:35 +0200

    Build works now

--- Emperor/src/tools/AuxiliaryParameters.cpp
+++ Emperor/src/tools/AuxiliaryParameters.cpp
@@ -1,25 +1,26 @@
-#-------------------------------------------------------------------------------
-# Copyright (c) 2013, TU Muenchen, Chair of Structural Analysis,
-# Stefan Sicklinger, Tianyang Wang, Munich. All rights reserved.
-# 
-# This file is part of EMPIRE.
-# 
-# EMPIRE is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# EMPIRE is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with EMPIRE.  If not, see http://www.gnu.org/licenses/.
-#-------------------------------------------------------------------------------
+/*  Copyright &copy; 2013, TU Muenchen, Chair of Structural Analysis,
+ *  Stefan Sicklinger, Tianyang Wang, Munich
+ *
+ *  All rights reserved.
+ *
+ *  This file is part of EMPIRE.
+ *
+ *  EMPIRE is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  EMPIRE is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with EMPIRE.  If not, see http://www.gnu.org/licenses/.
+ */
 #include "AuxiliaryParameters.h"
 #include <limits>
-#define GIT_SHA1 "77308e1903b7dd4bb0491787ca0cab5101ff7583" //Cmake will set this value
+#define GIT_SHA1 "7a84e0b782334d5dee0a027c30cb887bda4462b7" //Cmake will set this value
 
 namespace EMPIRE {
 
